bc97dc2a0ca097dc7ee9066452d91efb234e4fd2
Yunus <yunusdxn@hotmail.com>
2024-03-20 18:02:50 +0300

    Part D and Part E are accomplished

--- rsa.py
+++ rsa.py
@@ -1,27 +1,23 @@
 import helpingFunctions
 import random
-
 def rsa(p,q):
 
     n = p * q
 
     totientFunc = (p-1)*(q-1)
 
+    # Part E
     e = random.randint(2,totientFunc-1) # 1 < e < totientFunc
-
     while (helpingFunctions.euclidGCD(e,totientFunc)!=1):
         e = random.randint(2,totientFunc-1)
 
-    # Debugger to see variables
-    #print(f"p: {p}, q: {q}, n: {n}, totientFunc: {totientFunc}, e: {e}")
-
+    # Part D
     d = random.randint(0,n) # 0 < d < n
 
-    c = e*d
-
-    while (helpingFunctions.euclidGCD(e,d)!=1 and c!=1%n):
+    while ((e*d) % totientFunc != 1):
         d = random.randint(0,n)
+        if (d > totientFunc):
+            d = d-totientFunc
 
-
-    # Debugger to see variables
+    # Debugger to see variables **************************************************************
     print(f"p: {p}, q: {q}, n: {n}, totientFunc: {totientFunc}, e: {e}, d: {d}")
