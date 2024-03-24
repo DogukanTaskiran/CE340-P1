65e2ea973e8b6f6cdc4b7a42e1f9351e612607b0
DogukanTaskiran <dogukantaskiran1222@gmail.com>
2024-03-24 23:00:31 +0300

    SDES Demo

--- SDES.py
+++ SDES.py
@@ -2,9 +2,26 @@ import random
 
 # G.2 S-DES KEY GENERATION
 
-# P10(k1, k2, k3, k4, k5, k6, k7, k8, k9, k10) = (k3, k5, k2, k7, k4, k10, k1, k9, k8, k6) 
+# Expansion Permutation (4, 1, 2, 3, 2, 3, 4, 1)
+expansionPermutation = [3, 0, 1, 2, 1, 2, 3, 0]
+
+# P4 Permutation (2,4,3,1)
+p4_permutation = [1, 3, 2, 0]
+
+# P10 Permutation (k1, k2, k3, k4, k5, k6, k7, k8, k9, k10) = (k3, k5, k2, k7, k4, k10, k1, k9, k8, k6) 
+p10_order = [2, 4, 1, 6, 3, 9, 0, 8, 7, 5] 
+
+# P8 Permutation (6,3,7,4,8,5,10,9)
+p8_order = [5, 2, 6, 3, 7, 4, 9, 8]
+
+# IP 2 6 3 1 4 8 5 7
+ipOrder = [1, 5, 2, 0, 3, 7, 4, 6]
+
+# IP^(–1) 4 1 3 5 7 2 8 6
+ipInverseOrder = [3, 0, 2, 4, 6, 1, 7, 5]
+
+
 def permute10(key):
-    p10_order = [2, 4, 1, 6, 3, 9, 0, 8, 7, 5] 
     p10_key = [key[i] for i in p10_order]
     return p10_key
 
@@ -14,104 +31,100 @@ def split_key(p10_key):
     rightHalf = p10_key[5:]
     return leftHalf, rightHalf
 
-def one_bit_left_shift(half):
-    shifted_half = half[1:] + [half[0]]
-    return shifted_half
+def oneBitLeftShift(half):
+    shiftedHalf = half[1:] + [half[0]]
+    return shiftedHalf
 
-# P8(6,3,7,4,8,5,10,9)
 def permute8(shiftedLeft, shiftedRight):
     combined_key = shiftedLeft + shiftedRight
-    p8_order = [5, 2, 6, 3, 7, 4, 9, 8]
-    subkey = [combined_key[i] for i in p8_order]
-    # Key1
-    return subkey
+    subkey = [combined_key[i-1] for i in p8_order]
+    return subkey # Key 1
+
+# Random key of 10 bits
+def keyGenerator():
+    key = ""
+    for i in range(10):
+        key += str(random.randint(0, 1))
+    return key
 
 def generateSubkeys(key):
     p10_key = permute10(key)
     leftHalf, rightHalf = split_key(p10_key)
 
-    shiftedLeft = one_bit_left_shift(leftHalf)
-    shiftedRight = one_bit_left_shift(rightHalf)
+    shiftedLeft = oneBitLeftShift(leftHalf)
+    shiftedRight = oneBitLeftShift(rightHalf)
 
     k1 = permute8(shiftedLeft, shiftedRight)
  
-    shiftedLeft = one_bit_left_shift(leftHalf)
-    shiftedRight = one_bit_left_shift(rightHalf)
+    shiftedLeft = oneBitLeftShift(leftHalf)
+    shiftedRight = oneBitLeftShift(rightHalf)
 
-    shiftedLeft = one_bit_left_shift(shiftedLeft)
-    shiftedRight = one_bit_left_shift(shiftedRight)
+    shiftedLeft2 = oneBitLeftShift(shiftedLeft)
+    shiftedRight2 = oneBitLeftShift(shiftedRight)
 
-    k2 = permute8(shiftedLeft, shiftedRight)
+    k2 = permute8(shiftedLeft2, shiftedRight2)
 
     return k1, k2
 
 # G.3 S-DES ENCRYPTION
 
-# IP 2 6 3 1 4 8 5 7
 def initialPermutation(message):
-    ipOrder = [1, 5, 2, 0, 3, 7, 4, 6]
     permutedText = [message[i - 1] for i in ipOrder]
     return permutedText
 
-# IP^(–1) 4 1 3 5 7 2 8 6
 def inversePermutation(ciphertext):
-    ipInverseOrder = [3, 0, 2, 4, 6, 1, 7, 5]
     inversePermutedText = [ciphertext[i - 1] for i in ipInverseOrder]
     return inversePermutedText
 
 # fk(L,R) = (L xor F(R,SK),R)
 
-def split_message(message):
-    half_len = len(message) // 2
-    left_half = message[:half_len]
-    right_half = message[half_len:]
-    return left_half, right_half
-
-def binary_to_string(binary_list):
-    # Ikili listeyi 8 bitlik parçalara ayır
-    chunks = [binary_list[i:i+8] for i in range(0, len(binary_list), 8)]
-    # Her 8 bitlik parçayı ASCII karakterine dönüştür ve birleştir
-    string = ''.join([chr(int(''.join(map(str, chunk)), 2)) for chunk in chunks])
-    return string
-
-def string_to_binary(string):
-    binary_list = []
-    for char in string:
-        binary_char = bin(ord(char))[2:].zfill(8)
-        binary_list.extend([int(bit) for bit in binary_char])
-    return binary_list
+def splitMessage(message):
+    halfLenght = len(message) // 2
+    leftHalf = message[:halfLenght]
+    rightHalf = message[halfLenght:]
+    return leftHalf, rightHalf
 
 def encrypt(message, key):
-    k1, k2 = generateSubkeys(key)
-    ip_message = initialPermutation(message)
-    left_half, right_half = split_message(ip_message)
+
+    # Generating k1 and k2
+    k1, k2 = generateSubkeys(key)    
+    
+    # Initial permutation
+    ipMessage = initialPermutation(message)
+    leftHalf, rightHalf = splitMessage(ipMessage)
 
     # Round 1
-    f_result = feistel_function(right_half, k1)
-    new_right = xor(left_half, f_result)
+    fResult = feistelFunction(rightHalf, k1)
+    newRight = xor(leftHalf, fResult)
     # Round 2
-    f_result = feistel_function(new_right, k2)
-    new_left = xor(right_half, f_result)
+    fResult = feistelFunction(newRight, k2)
+    newLeft = xor(rightHalf, fResult)
 
     # Final Permutation
-    encrypted_text = inversePermutation(new_left + new_right)
-    return encrypted_text
+    encryptedText = inversePermutation(newLeft + newRight)
+    return encryptedText
 
 def decrypt(ciphertext, key):
-    k1, k2 = generateSubkeys(key)
-    ip_ciphertext = initialPermutation(ciphertext)
-    left_half, right_half = split_message(ip_ciphertext)
 
-        # Round 1
-    f_result = feistel_function(right_half, k2)
-    new_right = xor(left_half, f_result)
+    # Generating k1 and k2
+    k1, k2 = generateSubkeys(key)
+
+    # Initial permutation
+    ip = initialPermutation(ciphertext)
+
+    # Split half
+    leftHalf, rightHalf = splitMessage(ip)
+
+    # Round 1
+    fResult = feistelFunction(rightHalf, k2)
+    newRight = xor(leftHalf, fResult)
     # Round 2
-    f_result = feistel_function(new_right, k1)
-    new_left = xor(right_half, f_result)
+    fResult = feistelFunction(newRight, k1)
+    newLeft = xor(rightHalf, fResult)
 
     # Final Permutation
-    decrypted_text = inversePermutation(new_left + new_right)
-    return decrypted_text
+    decryptedText = inversePermutation(newLeft + newRight)
+    return decryptedText
 
 
 # S-Box Definitions
@@ -129,58 +142,51 @@ sbox1 = [
     [2, 1, 0, 3]
 ]
 
-# Expansion Permutation
-expansion_permutation = [3, 0, 1, 2, 1, 2, 3, 0]
-
-# P4 Permutation
-p4_permutation = [1, 3, 2, 0]
+# Switch Function
+def switchFunction(bits):
+    return bits[4:] + bits[:4]
 
 def expansion(text):
-    expanded_text = [text[i] for i in expansion_permutation]
-    return expanded_text
+    return ''.join(str(text[i-1]) for i in expansionPermutation)
 
+
+# List based xor
 def xor(left, right):
     return [left[i] ^ right[i] for i in range(len(left))]
 
-def sbox_substitution(text, sbox):
+# Integer sbox substitution
+def sboxSubstitution(text, sbox):
     row = int(''.join(map(str, [text[0], text[3]])), 2)
     col = int(''.join(map(str, [text[1], text[2]])), 2)
     return [int(x) for x in format(sbox[row][col], '02b')]
 
-def p4_permute(text):
-    permuted_text = [text[i] for i in p4_permutation]
-    return permuted_text
+# P4 Permutation
+def p4Permute(text):
+    permutedText = [text[i] for i in p4_permutation]
+    return permutedText
 
-def feistel_function(right, subkey):
-    expanded_right = expansion(right)
-    xor_result = xor(expanded_right, subkey)
-    sbox0_result = sbox_substitution(xor_result[:4], sbox0)
-    sbox1_result = sbox_substitution(xor_result[4:], sbox1)
-    sbox_result = sbox0_result + sbox1_result
-    permuted_result = p4_permute(sbox_result)
-    return permuted_result
+def feistelFunction(ip, subkey):
 
-def string_to_binary(string):
-    binary_list = []
-    for char in string:
-        binary_char = bin(ord(char))[2:].zfill(8)
-        binary_list.extend([int(bit) for bit in binary_char])
-    return binary_list
+    leftIp = ip[:4]
+    rightIp = ip[4:]
 
-def main():
-    message = input("Enter the message: ")
-    key = input("Enter the key (10-bit binary): ")
+    expandedRight = expansion(rightIp)
+    xorResult = xor(expandedRight, subkey)
+    sbox0Result = sboxSubstitution(xorResult[:4], sbox0)
+    sbox1Result = sboxSubstitution(xorResult[4:], sbox1)
+    sboxResult = sbox0Result + sbox1Result
+    permutedResult = p4Permute(sboxResult)
+    
+    # fK(L, R) = (L xor F(R, SK), R)
 
-    # Convert string message to binary
-    binary_message = string_to_binary(message)
-    key_binary = [int(bit) for bit in key]
+    F = (xor(leftIp,permutedResult))
 
-    print("Encrypting using key:", key)  # Anahtarı print et
+    return F,rightIp
 
-    # Encrypt the message
-    encrypted_message = encrypt(binary_message, key_binary)
-    print("Encrypted Message:", binary_to_string(encrypted_message))
+# Converting string to binary
+def stringToBinary(input_string):
+    return ''.join(format(ord(char), '08b') for char in input_string)
 
-    # Decrypt the message
-    decrypted_message = decrypt(encrypted_message, key_binary)
-    print("Decrypted Message:", binary_to_string(decrypted_message))
+# Converting binary to string
+def binaryToString(input_binary):
+    return ''.join(chr(int(input_binary[i:i+8], 2)) for i in range(0, len(input_binary), 8))
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import RSA
 import SDES
 
+# RSA
+
 publicKey, privateKey = RSA.keyGenerator()
 
 message = "DENEME123!"
@@ -11,35 +13,29 @@ decryptedMessage = RSA.decrypt(privateKey, encryptedMessage)
 print("Decrypted:", decryptedMessage)
 
 print()
-print("SDES Keys")
 
-# Debugger to see values of keys (Geçici deneme)
-sdesKeys = SDES.generateSubkeys([1, 0, 1, 0, 0, 0, 0, 0, 1, 0])
-print(sdesKeys)
+# S-DES
 
-# Example usage:
-plaintext = [1, 0, 1, 0, 0, 0, 1, 1]  # Example 8-bit plaintext block
-permuted_plaintext = SDES.initialPermutation(plaintext)
-print("Initial Permutation:", permuted_plaintext)
+    # Generate a random key
+key = SDES.keyGenerator()
+print("Generated Key:", key)
 
-# Example usage for inverse permutation:
-ciphertext = [0, 1, 1, 0, 1, 0, 0, 1]  # Example 8-bit ciphertext block
-inverse_permuted_ciphertext = SDES.inversePermutation(ciphertext)
-print("Inverse Permutation:", inverse_permuted_ciphertext)
+    # Message to encrypt
+message = "Hello, World!"
+print("Original Message:", message)
 
-message = input("Enter the message: ")
-key = input("Enter the key (10-bit binary): ")
-
-# Convert string message to binary
-binary_message = SDES.string_to_binary(message)
-key_binary = [int(bit) for bit in key]
-
-print("Encrypting using key:", key)  # Anahtarı print et
+    # Convert message to binary
+binary_message = SDES.stringToBinary(message)
+print("Binary Message:", binary_message)
 
     # Encrypt the message
-encrypted_message = SDES.encrypt(binary_message, key_binary)
-print("Encrypted Message:", SDES.binary_to_string(encrypted_message))
+encrypted_message = SDES.encrypt(binary_message, key)
+print("Encrypted Message:", encrypted_message)
 
     # Decrypt the message
-decrypted_message = SDES.decrypt(encrypted_message, key_binary)
-print("Decrypted Message:", SDES.binary_to_string(decrypted_message))
+decrypted_message = SDES.decrypt(encrypted_message, key)
+print("Decrypted Message:", decrypted_message)
+
+    # Convert decrypted binary message to string
+decrypted_string = SDES.binaryToString(decrypted_message)
+print("Decrypted String:", decrypted_string)
